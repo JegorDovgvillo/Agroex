7210a61799fc12e253a19acce0ba7bda82c71bdf
Iryna Kanavalchuk <irkinbk@gmail.com>
2024-01-26 17:07:46 +0300

    add fixes #24

--- src/components/timeCountDown/index.jsx
+++ src/components/timeCountDown/index.jsx
@@ -1,6 +1,5 @@
-import { Box, Typography } from '@mui/material';
-import timeIcon from '../../assets/icons/time.svg';
-import styles from './time.module.scss';
+import timeIcon from "../../assets/icons/time.svg";
+import styles from "./time.module.scss";
 
 export const TimeCountDown = ({ creationDateStr, expirationDateStr }) => {
   const creationDate = new Date(creationDateStr);
@@ -10,17 +9,14 @@ export const TimeCountDown = ({ creationDateStr, expirationDateStr }) => {
   const hours = Math.floor(
     (durationMs % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000)
   );
-  const minutes = Math.floor((durationMs % (60 * 60 * 1000)) / (60 * 1000));
 
-  const getTimeStr = () =>
-    days >= 1 ? `${days}d ${hours}h` : `${hours}h ${minutes}m`;
-
+  const getTimeStr = () => (days >= 1 ? `${days}d ${hours}h` : `${hours}h`);
   const time = getTimeStr();
 
   return (
-    <Box className={styles.container}>
+    <div className={styles.container}>
       <img src={timeIcon} alt="Watch icon"></img>
-      {time && <Typography className={styles.time}>{time}</Typography>}
-    </Box>
+      {time && <span className={styles.time}>{time}</span>}
+    </div>
   );
 };
