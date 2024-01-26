778add7fb9f18bcdd95eb9088ef7454b83a2a200
Egor Dovgvillo <filimonchik1998@mail.ru>
2024-01-26 17:55:18 +0300

    add fixes #28

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Router from "./helpers/router";
-import { ThemeProvider } from "@mui/material/styles";
-import { customTheme } from "./theme";
-import { Provider } from "react-redux";
-import store from "./store/store";
+import Router from './helpers/router';
+import { ThemeProvider } from '@mui/material/styles';
+import { customTheme } from './theme';
+import { Provider } from 'react-redux';
+import store from './store/store';
 function App() {
   return (
     <ThemeProvider theme={customTheme}>
