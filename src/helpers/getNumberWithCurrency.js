a2a61a626780c32f0961713a8b0bab4256a1268b
Egor Dovgvillo <filimonchik1998@mail.ru>
2024-01-29 20:09:07 +0300

    delete files

--- src/helpers/getNumberWithCurrency.js
+++
@@ -1,8 +0,0 @@
-const getNumberWithCurrency = (number, currency, locale = 'en') => {
-  return new Intl.NumberFormat(locale, {
-    style: 'currency',
-    currency: currency,
-  }).format(number);
-};
-
-export default getNumberWithCurrency;
