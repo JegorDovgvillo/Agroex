0aff09dceefbe74a84bdd01df7a17a3d188ea510
Egor Dovgvillo <filimonchik1998@mail.ru>
2024-02-06 15:52:03 +0300

    add env file, add new path aliases, add lodash #41

--- src/helpers/endpoints.js
+++ src/helpers/endpoints.js
@@ -1,14 +1,14 @@
-export const BASE_URL = 'http://localhost:8080';
-export const IMAGE_URL = 'http://localhost:9000';
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const IMAGE_URL = import.meta.env.VITE_BASE_IMAGE_URL;
 
 const ENDPOINTS = {
-  LOTS: '/lots',
-  LOTS_DETAILS: '/lots/details',
-  USERS: '/users',
-  ALL_CATEGORIES: '/categories/all',
-  MAIN_CATEGORIES: '/categories',
-  COUNTRIES: '/countries',
-  IMAGES: '/images',
+  IMAGES: import.meta.env.VITE_IMAGES,
+  LOTS: import.meta.env.VITE_LOTS_ENDPOINT,
+  LOTS_DETAILS: import.meta.env.VITE_LOTS_DETAILS_ENDPOINT,
+  USERS: import.meta.env.VITE_USERS,
+  ALL_CATEGORIES: import.meta.env.VITE_ALL_CATEGORIES,
+  MAIN_CATEGORIES: import.meta.env.VITE_MAIN_CATEGORIES,
+  COUNTRIES: import.meta.env.VITE_COUNTRIES,
 };
 
 export default ENDPOINTS;
