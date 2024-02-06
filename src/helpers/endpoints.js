e764ebcbeee7910b6d2890d7317c7ab7d6e5165b
Egor Dovgvillo <filimonchik1998@mail.ru>
2024-02-06 16:40:04 +0300

    add fixes #41

--- src/helpers/endpoints.js
+++ src/helpers/endpoints.js
@@ -2,13 +2,13 @@ export const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const IMAGE_URL = import.meta.env.VITE_BASE_IMAGE_URL;
 
 const ENDPOINTS = {
-  IMAGES: import.meta.env.VITE_IMAGES,
-  LOTS: import.meta.env.VITE_LOTS_ENDPOINT,
-  LOTS_DETAILS: import.meta.env.VITE_LOTS_DETAILS_ENDPOINT,
-  USERS: import.meta.env.VITE_USERS,
-  ALL_CATEGORIES: import.meta.env.VITE_ALL_CATEGORIES,
-  MAIN_CATEGORIES: import.meta.env.VITE_MAIN_CATEGORIES,
-  COUNTRIES: import.meta.env.VITE_COUNTRIES,
+  IMAGES: '/images',
+  LOTS: '/lots',
+  LOTS_DETAILS: '/lots/details',
+  USERS: '/users',
+  ALL_CATEGORIES: '/categories/all',
+  MAIN_CATEGORIES: '/categories',
+  COUNTRIES: '/countries',
 };
 
 export default ENDPOINTS;
