c149134898a6fe535658321a0d156dbc38ab7c31
Egor Dovgvillo <filimonchik1998@mail.ru>
2024-02-08 18:23:35 +0300

    work with drag and drop #48

--- src/components/dragAndDrop/index.jsx
+++ src/components/dragAndDrop/index.jsx
@@ -1,95 +1,10 @@
-// import React, { useEffect, useState } from 'react';
-// import { useDropzone } from 'react-dropzone';
-
-// const thumbsContainer = {
-//   display: 'flex',
-//   flexDirection: 'row',
-//   flexWrap: 'wrap',
-//   marginTop: 16,
-// };
-
-// const thumb = {
-//   display: 'inline-flex',
-//   borderRadius: 2,
-//   border: '1px solid #eaeaea',
-//   marginBottom: 8,
-//   marginRight: 8,
-//   width: 100,
-//   height: 100,
-//   padding: 4,
-//   boxSizing: 'border-box',
-// };
-
-// const thumbInner = {
-//   display: 'flex',
-//   minWidth: 0,
-//   overflow: 'hidden',
-// };
-
-// const img = {
-//   display: 'block',
-//   width: 'auto',
-//   height: '100%',
-// };
-
-// function Previews(props) {
-//   const [files, setFiles] = useState([]);
-//   const { getRootProps, getInputProps } = useDropzone({
-//     maxFiles: 3,
-//     accept: {
-//       'image/png': ['.png'],
-//       'text/html': ['.html', '.htm'],
-//     },
-//     onDrop: (acceptedFiles) => {
-//       setFiles(
-//         acceptedFiles.map((file) =>
-//           Object.assign(file, {
-//             preview: URL.createObjectURL(file),
-//           })
-//         )
-//       );
-//     },
-//   });
-
-//   const thumbs = files.map((file) => (
-//     <div style={thumb} key={file.name}>
-//       <div style={thumbInner}>
-//         <img
-//           src={file.preview}
-//           style={img}
-//           // Revoke data uri after image is loaded
-//           onLoad={() => {
-//             URL.revokeObjectURL(file.preview);
-//           }}
-//         />
-//       </div>
-//     </div>
-//   ));
-
-//   useEffect(() => {
-//     // Make sure to revoke the data uris to avoid memory leaks, will run when files change
-//     return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
-//   }, [files]);
-// console.log(files)
-//   return (
-//     <section className="container">
-//       <div {...getRootProps({ className: 'dropzone' })}>
-//         <input {...getInputProps()} />
-//         <p>Drag 'n' drop some files here, or click to select files</p>
-//       </div>
-//       <aside style={thumbsContainer}>{thumbs}</aside>
-//     </section>
-//   );
-// }
-
-// export default Previews;
 import { useCallback, useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 
 const Previews = ({ className }) => {
   const [files, setFiles] = useState([]);
 
-  const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
+  const onDrop = useCallback((acceptedFiles) => {
     if (acceptedFiles?.length) {
       setFiles((previousFiles) => [
         ...previousFiles,
@@ -97,10 +12,6 @@ const Previews = ({ className }) => {
           Object.assign(file, { preview: URL.createObjectURL(file) })
         ),
       ]);
-    }
-
-    if (rejectedFiles?.length) {
-      setRejected((previousFiles) => [...previousFiles, ...rejectedFiles]);
     }
   }, []);
 
@@ -113,7 +24,6 @@ const Previews = ({ className }) => {
   });
 
   useEffect(() => {
-    // Revoke the data uris to avoid memory leaks
     return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
   }, [files]);
 
@@ -127,25 +37,29 @@ const Previews = ({ className }) => {
     if (!files?.length) return;
 
     const formData = new FormData();
-    files.forEach((file) => formData.append('file', file));
-    formData.append('upload_preset', 'friendsbook');
+    files.forEach((file) => formData.append(`file`, file));
+    formData.append('somth', e.target.somth.value);
+    formData.append('somth1', '52');
 
-    const URL = process.env.NEXT_PUBLIC_CLOUDINARY_URL;
-    const data = await fetch(URL, {
-      method: 'POST',
-      body: formData,
-    }).then((res) => res.json());
-
-    console.log(data);
+    const URL = 'http://localhost:8080/lots';
+    try {
+      const response = await fetch(URL, {
+        method: 'POST',
+        body: formData,
+        // headers: {
+        //   'Content-Type': 'multipart/form-data',
+        // },
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div
-        {...getRootProps({
-          className: className,
-        })}
-      >
+      <div {...getRootProps({ className: className })}>
         <input {...getInputProps()} />
         <div>
           {isDragActive ? (
@@ -155,14 +69,12 @@ const Previews = ({ className }) => {
           )}
         </div>
       </div>
-
-      {/* Preview */}
       <section>
         <div>
           <h2>Preview</h2>
         </div>
-        {/* Accepted files */}
         <h3>Accepted Files</h3>
+        <input name="somth" type="hidden" value={12345}></input>
         <ul>
           {files.map((file) => (
             <li key={file.name}>
@@ -171,21 +83,133 @@ const Previews = ({ className }) => {
                 alt={file.name}
                 width={100}
                 height={100}
-                onLoad={() => {
-                  URL.revokeObjectURL(file.preview);
-                }}
+                onLoad={() => URL.revokeObjectURL(file.preview)}
               />
-              <button
-                type="button"
-                onClick={() => removeFile(file.name)}
-              ></button>
+              <button type="button" onClick={() => removeFile(file.name)}>
+                Remove
+              </button>
               <p>{file.name}</p>
             </li>
           ))}
         </ul>
       </section>
+      <button type="submit">Submit</button>
     </form>
   );
 };
 
 export default Previews;
+
+// import { useCallback, useEffect, useState } from 'react';
+// import { useDropzone } from 'react-dropzone';
+
+// const Previews = ({ className }) => {
+//   const [files, setFiles] = useState([]);
+
+//   const onDrop = useCallback((acceptedFiles) => {
+//     if (acceptedFiles?.length) {
+//       setFiles((previousFiles) => [
+//         ...previousFiles,
+//         ...acceptedFiles.map((file) =>
+//           Object.assign(file, { preview: URL.createObjectURL(file) })
+//         ),
+//       ]);
+//     }
+//   }, []);
+
+//   const { getRootProps, getInputProps, isDragActive } = useDropzone({
+//     accept: {
+//       'image/*': [],
+//     },
+//     maxSize: 1024 * 1000,
+//     onDrop,
+//   });
+
+//   useEffect(() => {
+//     // Revoke the data uris to avoid memory leaks
+//     return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
+//   }, [files]);
+
+//   const removeFile = (name) => {
+//     setFiles((files) => files.filter((file) => file.name !== name));
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     if (!files?.length) return;
+
+//     const formData = new FormData();
+//     files.forEach((file, index) => formData.append(`file`, file));
+//     formData.append('somth', e.target.somth.value);
+//     formData.append('somth1', '52');
+
+//     const URL = 'http://localhost:8080/lots/1/images';
+//     const data = await fetch(URL, {
+//       method: 'POST',
+//       body: formData,
+//       headers: {
+//         'Content-Type': 'multipart/form-data',
+//       },
+//     })
+//       .then((res) => console.log(res))
+//       .catch((res) => console.log(res));
+
+//     console.log(data);
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div
+//         {...getRootProps({
+//           className: className,
+//         })}
+//       >
+//         <input {...getInputProps()} />
+//         <div>
+//           {isDragActive ? (
+//             <>
+//               <p>Drop the files here ...</p>
+//             </>
+//           ) : (
+//             <p>Drag & drop files here, or click to select files</p>
+//           )}
+//         </div>
+//       </div>
+
+//       {/* Preview */}
+//       <section>
+//         <div>
+//           <h2>Preview</h2>
+//         </div>
+//         {/* Accepted files */}
+//         <h3>Accepted Files</h3>
+//         <input name="somth" type="hidden" value={12345}></input>
+//         <ul>
+//           {files.map((file) => (
+//             <li key={file.name}>
+//               <img
+//                 src={file.preview}
+//                 alt={file.name}
+//                 width={100}
+//                 height={100}
+//                 onLoad={() => {
+//                   URL.revokeObjectURL(file.preview);
+//                 }}
+//               />
+
+//               <button
+//                 type="button"
+//                 onClick={() => removeFile(file.name)}
+//               ></button>
+//               <p>{file.name}</p>
+//             </li>
+//           ))}
+//         </ul>
+//       </section>
+//       <button type="submit">eerr</button>
+//     </form>
+//   );
+// };
+
+// export default Previews;
