bcb236ae26879e5344ddad10f3d14d6231787f02
Iryna Kanavalchuk <irkinbk@gmail.com>
2024-02-11 20:52:42 +0300

    add react image gallery slider #46

--- src/components/customSlider/index.jsx
+++ src/components/customSlider/index.jsx
@@ -1,74 +1,27 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ImageGallery from 'react-image-gallery';
 
 import ENDPOINTS, { IMAGE_URL } from '@helpers/endpoints';
 
 const baseURL = `${IMAGE_URL}${ENDPOINTS.IMAGES}`;
-import styles from './customSlider.module.scss';
 
-const {
-  customSliderContainer,
-  customStickDots,
-  slide,
-  sliderImage,
-  arrow,
-  next,
-  prev,
-} = styles;
+import 'react-image-gallery/styles/css/image-gallery.css';
+import './customSlider.scss';
 
-function NextArrow(props) {
-  const { onClick } = props;
-
-  return (
-    <div className={`${arrow} ${next}`} onClick={onClick}>
-      <ArrowForwardIosIcon />
-    </div>
-  );
-}
-
-function PrevArrow(props) {
-  const { onClick } = props;
-
-  return (
-    <div className={`${arrow} ${prev}`} onClick={onClick}>
-      <ArrowForwardIosIcon />
-    </div>
-  );
-}
+const getImages = (images) => {
+  return images.map((image) => ({
+    original: `${baseURL}/${image.name}`,
+    originalAlt: 'Lot image',
+    thumbnail: `${baseURL}/${image.name}`,
+    thumbnailAlt: 'Lot image',
+  }));
+};
 
 const CustomSlider = ({ images }) => {
-  const settings = {
-    customPaging: function (i) {
-      return (
-        <a>
-          <img src={`${baseURL}/${images[i].name}`} />
-        </a>
-      );
-    },
-
-    fade: true,
-    dots: true,
-    dotsClass: `slick-dots slick-thumb ${customStickDots}`,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-  };
+  const imagesArr = getImages(images);
 
   return (
-    <div className={`${customSliderContainer}`}>
-      <Slider {...settings}>
-        {images.map((el) => (
-          <div key={el.id} className={slide}>
-            <img src={`${baseURL}/${el.name}`} className={sliderImage} />
-          </div>
-        ))}
-      </Slider>
+    <div className="customSliderContainer">
+      <ImageGallery items={imagesArr} infinite={false} />
     </div>
   );
 };
---
+++ src/components/customSlider/temp/index.jsx
@@ -0,0 +1,76 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+import ENDPOINTS, { IMAGE_URL } from '@helpers/endpoints';
+
+const baseURL = `${IMAGE_URL}${ENDPOINTS.IMAGES}`;
+import styles from './customSlider.module.scss';
+
+const {
+  customSliderContainer,
+  customStickDots,
+  slide,
+  sliderImage,
+  arrow,
+  next,
+  prev,
+} = styles;
+
+function NextArrow(props) {
+  const { onClick } = props;
+
+  return (
+    <div className={`${arrow} ${next}`} onClick={onClick}>
+      <ArrowForwardIosIcon />
+    </div>
+  );
+}
+
+function PrevArrow(props) {
+  const { onClick } = props;
+
+  return (
+    <div className={`${arrow} ${prev}`} onClick={onClick}>
+      <ArrowForwardIosIcon />
+    </div>
+  );
+}
+
+const CustomSlider = ({ images }) => {
+  const settings = {
+    customPaging: function (i) {
+      return (
+        <a>
+          <img src={`${baseURL}/${images[i].name}`} />
+        </a>
+      );
+    },
+
+    fade: true,
+    dots: true,
+    dotsClass: `slick-dots slick-thumb ${customStickDots}`,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  return (
+    <div className={`${customSliderContainer}`}>
+      <Slider {...settings}>
+        {images.map((el) => (
+          <div key={el.id} className={slide}>
+            <img src={`${baseURL}/${el.name}`} className={sliderImage} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default CustomSlider;
