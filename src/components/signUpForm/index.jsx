e3659f8d10c603576c62ec61a969eba0c01f0b93
Egor Dovgvillo <filimonchik1998@mail.ru>
2024-02-22 02:01:01 +0300

    add fixes, customize styles, create a time zones object #45

--- src/components/signUpForm/index.jsx
+++ src/components/signUpForm/index.jsx
@@ -1,28 +1,82 @@
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
-import { useNavigate } from 'react-router';
-import awsConfig from '@helpers/aws-config';
-
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+// import { Auth } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-import styles from './signUpForm.module.scss';
+
+import awsConfig from '@helpers/aws-config';
+import timeZones from '../../data/timeZones';
+
+import './signUpForm.scss';
 
 Amplify.configure(awsConfig);
 
+const formFields = {
+  signUp: {
+    email: {
+      order: 1,
+    },
+    name: {
+      order: 2,
+    },
+    password: {
+      order: 3,
+    },
+    confirm_password: {
+      order: 4,
+    },
+  },
+};
+
 const SignUpForm = () => {
-  const navigate = useNavigate();
-  const navigateToUserProfile = () => {
-    navigate('/lots');
-  };
+  const filteredTimeZone = timeZones.filter(
+    (zone) => zone.value === 'Europe/London'
+  );
+
+  // const services = {
+  //   async handleSignUp() {
+  //     return Auth.signUp({});
+  //   },
+  // };
+
   return (
-    <div className={styles.wrapp}>
+    <div className="wrapp">
       <Authenticator
+        // services={services}
+        formFields={formFields}
         signUpAttributes={['email', 'name']}
+        components={{
+          SignUp: {
+            FormFields() {
+              return (
+                <>
+                  <Authenticator.SignUp.FormFields />
+                  <Autocomplete
+                    disablePortal
+                    defaultValue={
+                      filteredTimeZone.length && filteredTimeZone[0]
+                    }
+                    options={timeZones}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => (
+                      <label>
+                        Time Zones
+                        <TextField {...params} name="zoneinfo" />
+                      </label>
+                    )}
+                  />
+                </>
+              );
+            },
+          },
+        }}
         // socialProviders={['google']}
       >
         {({ signOut, user }) => (
           <div>
             <p>Welcome {user.username}</p>
-            <button onClick={navigateToUserProfile}>Sign out</button>
+            <button onClick={signOut}>Sign out</button>
           </div>
         )}
       </Authenticator>
--- src/helpers/aws-config.js
+++ src/helpers/aws-config.js
@@ -3,17 +3,23 @@ const awsConfig = {
     Cognito: {
       userPoolClientId: '5n4iencc2c5afpos8edngcgp3t',
       userPoolId: 'us-east-1_q32CiA5C2',
+
       loginWith: {
         // oauth: {
-        //   domain: 'https://test-front.auth.us-east-1.amazoncognito.com',
+        //   domain: 'https://front-test.auth.us-east-1.amazoncognito.com',
         //   scopes: [
         //     'email',
         //     'openid',
         //     'profile',
         //     'aws.cognito.signin.user.admin',
         //   ],
-        //   redirectSignIn: ['http://localhost:5173/lots', ''],
-        //   redirectSignOut: ['http://localhost:5173/lots', ''],
+        //   redirectSignIn: [
+        //     'http://localhost:5173/sign-up',
+        //     'http://localhost:5173/lots',
+        //   ],
+        //   redirectSignOut:
+        // 'http://localhost:5173/sign-up',
+        //     'http://localhost:5173/lots',
         //   responseType: 'code',
         // },
         // username: 'false',
