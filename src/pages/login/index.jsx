6628a9274e38c6f36b5a9606b4dafa9be5e52822
Egor Dovgvillo <filimonchik1998@mail.ru>
2024-02-23 23:37:32 +0300

    delete unused code #45

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,6 @@
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import { signUp, confirmSignIn, fetchAuthSession } from '@aws-amplify/auth';
 import _ from 'lodash';
-
-import { setAccessToken, setIdToken } from '@slices/usersListSlice.js';
 
 import LoginForm from '@components/loginForm';
 
@@ -13,15 +10,11 @@ import timeZones from '../../data/timeZones.js';
 
 const Login = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
 
   const filteredTimeZone = _.filter(timeZones, { value: 'Europe/London' });
 
   const getTokens = async () => {
-    const { accessToken, idToken } = (await fetchAuthSession()).tokens ?? {};
-
-    dispatch(setAccessToken(accessToken.toString()));
-    dispatch(setIdToken(idToken.toString()));
+    return (await fetchAuthSession().tokens) ?? {};
   };
 
   const services = {
