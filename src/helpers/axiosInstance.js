e2c673d1b4826d0604851ca9848ffd8c98efc2cc
Egor Dovgvillo <filimonchik1998@mail.ru>
2024-02-23 23:29:40 +0300

    add fixes #45

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
+import _ from 'lodash';
 
 import getToken from '@helpers/getToken';
 
 import { BASE_URL } from './endpoints';
+
+function getCookie() {
+  const cookies = _.map(document.cookie.split(';'), (cookie) =>
+    cookie.split('=')
+  );
+  const cookie = _.find(cookies, ([name]) =>
+    _.includes(name.trim(), 'idToken')
+  );
+  return cookie ? cookie[1] : null;
+}
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
