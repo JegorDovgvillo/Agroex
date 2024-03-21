f32b7e13c783a2bc31c96a57e3ded67d9d5ef2be
Egor Dovgvillo <filimonchik1998@mail.ru>
2024-03-21 22:49:49 +0300

    add image to lot update #143

--- src/components/customModals/modalForUpdaitingCategory.jsx
+++ src/components/customModals/modalForUpdaitingCategory.jsx
@@ -2,12 +2,20 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { Formik, Form } from 'formik';
+import { useState, useEffect, useRef } from 'react';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { v4 as uuidv4 } from 'uuid';
+import CloseIcon from '@mui/icons-material/Close';
+import _ from 'lodash';
+
+import { updateCategory } from '@thunks/fetchCategories';
 
 import { toggleModal } from '@slices/modalSlice';
 import { selectModalState } from '@slices/modalSlice';
-import { updateCategory } from '@thunks/fetchCategories';
 import { selectCategoryById } from '@slices/categoriesSlice';
 
+import ENDPOINTS, { IMAGE_URL } from '@helpers/endpoints';
 import { categoryTitleValidationSchema } from '@helpers/validationSchemes/lotValidationSchemes';
 
 import CustomTextField from '@customTextField';
@@ -15,8 +23,14 @@ import { CustomButton } from '@buttons/CustomButton';
 
 import styles from './infoModal.module.scss';
 
+
 const ModalForUpdatingCategory = () => {
   const dispatch = useDispatch();
+  const fileInputRef = useRef();
+
+  const [file, setFile] = useState(null);
+  const [iamgeSrc, setImageSrc] = useState(null);
+
   const isOpen = useSelector((state) =>
     selectModalState(state, 'updatingModal')
   );
@@ -31,28 +45,72 @@ const ModalForUpdatingCategory = () => {
     parentId: categoryFields?.parentId || '',
   };
 
-  const handleSubmitClick = (values) => {
-    dispatch(updateCategory({ id: categoryId, categoryData: values }));
-    dispatch(toggleModal('updatingModal'));
+  const handleSubmitClick = (values, { resetForm }) => {
+    const formData = new FormData();
+    const data = {
+      id: categoryFields.id,
+      title: values.title,
+      parentId: categoryFields.parentId,
+      image: categoryFields.image,
+    };
+
+    formData.append('file', file);
+    formData.append('data', JSON.stringify(data));
+    dispatch(updateCategory({ id: categoryId, categoryData: formData }));
+
+    resetForm();
   };
+
+  const handleFileChange = (event) => {
+    const newFile = event.target.files[0];
+
+    setFile(
+      _.assign(newFile, {
+        preview: URL.createObjectURL(newFile),
+        id: uuidv4(),
+      })
+    );
+  };
+
+  const closePopup = () => {
+    dispatch(toggleModal('updatingModal'));
+    setFile(null);
+    setImageSrc(null);
+  };
+
+  const removeFile = () => {
+    if (file) {
+      fileInputRef.current.value = '';
+
+      setFile(null);
+      URL.revokeObjectURL(file.preview);
+    }
+  };
+
+  useEffect(() => {
+    if (file || (categoryFields && categoryFields.image)) {
+      const src = file
+        ? file.preview
+        : `${IMAGE_URL}${ENDPOINTS.IMAGES}/${categoryFields.image}`;
+
+      setImageSrc(src);
+    }
+  }, [file, categoryFields]);
 
   return (
     <div>
       <Modal
         open={isOpen}
-        onClose={() => {
-          dispatch(toggleModal('updatingModal'));
-        }}
+        onClose={closePopup}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
         <Box className={styles.wrapp}>
           <h2 className={styles.title}>Update category</h2>
+          <CloseIcon className={styles.closeIcon} onClick={closePopup} />
           <Formik
             initialValues={categoryData}
-            onSubmit={async (values) => {
-              handleSubmitClick(values);
-            }}
+            onSubmit={handleSubmitClick}
             validationSchema={categoryTitleValidationSchema}
           >
             {({ values, errors, touched, isValid }) => (
@@ -65,13 +123,39 @@ const ModalForUpdatingCategory = () => {
                   value={values.title}
                   errors={errors.title}
                   touched={touched.title}
+                  type="modalTextField"
                 />
-                <CustomButton
-                  disabled={!isValid}
-                  text="Update"
-                  width="210px"
-                  typeOfButton="submit"
-                />
+                <div className={styles.buttonsWrapp}>
+                  <p>Selected image:</p>
+                  <img src={iamgeSrc} alt="Uploaded" />
+                  <CloseIcon
+                    onClick={removeFile}
+                    className={styles.deleteIcon}
+                  />
+                  <div className={styles.buttons}>
+                    <Button
+                      component="label"
+                      variant="contained"
+                      sx={{ width: '210px' }}
+                    >
+                      <CloudUploadIcon />
+                      <span>Upload file</span>
+                      <input
+                        ref={fileInputRef}
+                        type="file"
+                        style={{ display: 'none' }}
+                        onChange={handleFileChange}
+                        accept="image/*"
+                      />
+                    </Button>
+                    <CustomButton
+                      disabled={!isValid}
+                      text="Update"
+                      width="210px"
+                      typeOfButton="submit"
+                    />
+                  </div>
+                </div>
               </Form>
             )}
           </Formik>
--- src/components/customTabPanels/homePageTabPanel/subcategoryBanner/index.jsx
+++ src/components/customTabPanels/homePageTabPanel/subcategoryBanner/index.jsx
@@ -1,22 +1,24 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './banner.module.scss';
 import bannerImage from '@assets/images/banner.png';
-
+import ENDPOINTS, { IMAGE_URL } from '@helpers/endpoints';
 const { container, titleRow } = styles;
 
-const SubcategoryBanner = ({ title, path }) => {
+const SubcategoryBanner = ({ title, path, image }) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
     navigate(path);
   };
 
+  const src = image ? `${IMAGE_URL}${ENDPOINTS.IMAGES}/${image}` : bannerImage;
+
   return (
     <div className={container} onClick={handleClick}>
       <div className={titleRow}>
         <h4>{title}</h4>
       </div>
-      <img src={bannerImage} />
+      <img src={src} />
     </div>
   );
 };
