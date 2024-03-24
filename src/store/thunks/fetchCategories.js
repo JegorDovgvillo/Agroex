2193651ad4f86596a87316829f775482223b814c
Iryna Kanavalchuk <irkinbk@gmail.com>
2024-03-24 18:59:45 +0300

    add fixes #56

--- src/store/thunks/fetchCategories.js
+++ src/store/thunks/fetchCategories.js
@@ -3,15 +3,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axiosInstance from '@helpers/axiosInstance';
 import ENDPOINTS from '@helpers/endpoints';
 
-/* export const fetchCategories = createAsyncThunk(
-  'categories/fetchCategories',
-  async () => {
-    const response = await axiosInstance.get(ENDPOINTS.CATEGORIES);
-
-    return response.data;
-  }
-);
- */
 export const deleteCategory = createAsyncThunk(
   'categories/deleteCategory',
   async ({ id }, { rejectWithValue }) => {
